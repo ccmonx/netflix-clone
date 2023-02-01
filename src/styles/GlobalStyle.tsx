835cdb287b8bd1fd1f481f8864391b05a817be69
ccmonx <ccmonx@gmail.com>
2023-02-01 17:56:52 +0900

    [Style] netflix 로컬폰트 추가

--- src/styles/GlobalStyle.tsx
+++ src/styles/GlobalStyle.tsx
@@ -1,7 +1,27 @@
 import { createGlobalStyle } from "styled-components";
 import reset from "styled-reset";
+import NetflixSansBold from "../fonts/NetflixSans-Bold.woff2";
+import NetflixSansLight from "../fonts/NetflixSans-Light.woff2";
+import NetflixSansMedium from "../fonts/NetflixSans-Medium.woff2";
+import NetflixSansRegular from "../fonts/NetflixSans-Regular.woff2";
 
 export const GlobalStyle = createGlobalStyle`
+ @font-face {
+  font-family: NetflixSansBold;
+  src: url(${NetflixSansBold}) format("woff2");
+ }
+ @font-face {
+  font-family: NetflixSansLight;
+  src: url(${NetflixSansLight}) format("woff2");
+ }
+ @font-face {
+  font-family: NetflixSansMedium;
+  src: url(${NetflixSansMedium}) format("woff2");
+ }
+ @font-face {
+  font-family: NetflixSansRegular;
+  src: url(${NetflixSansRegular}) format("woff2");
+ }
  ${reset}
  * {
   box-sizing: border-box;
@@ -9,6 +29,7 @@ export const GlobalStyle = createGlobalStyle`
  body {
   background-color: ${(props) => props.theme.white.lighter};
   color: ${(props) => props.theme.black.lighter};
+  font-family: NetflixSansMedium, sans-serif;
   line-height: 1.4;
  }
  a {

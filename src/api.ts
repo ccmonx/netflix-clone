dcfecc4786187fa829cee26a2195573f722b9785
ccmonx <ccmonx@gmail.com>
2023-02-02 16:55:46 +0900

    [Home] API Data 의 타입 정의

--- src/api.ts
+++ src/api.ts
@@ -1,5 +1,29 @@
 export const API_KEY = "a5d7ef12614e13273c72a3743f1776c1";
 export const BASE_PATH = "https://api.themoviedb.org/3";
+/**
+ * 🔻 Query(getMovies)로 응답(전달)받은 Data의 타입을 정의하기
+ *  1. 웹브라우저 주소창에 쿼리를 입력하여 Data를 확인한다
+ *  2. 사용할 데이터의 형식에 맞게 타입을 정의한다(객체 내부의 배열,객체 확인)
+ *  3. 사용될 컴포넌트의 useQuery에 적용한다
+ */
+export interface IMovie {
+	id: number;
+	backdrop_path: string;
+	poster_path: string;
+	title: string;
+	overview: string;
+}
+
+export interface IGetMoviesResult {
+	dates: {
+		maximum: string;
+		ninimum: string;
+	};
+	page: number;
+	results: IMovie[];
+	total_pages: number;
+	total_results: number;
+}
 
 export function getMoives() {
 	return fetch(
--- src/routes/Home.tsx
+++ src/routes/Home.tsx
@@ -1,5 +1,5 @@
 import { useQuery } from "@tanstack/react-query";
-import { getMoives } from "../api";
+import { getMoives, IGetMoviesResult } from "../api";
 
 function Home() {
 	/**
@@ -9,7 +9,10 @@ function Home() {
 	 *  3. 비동기함수  : fetch('Data Address).then(json)
 	 *  4. useQuery : 2개의 파라미터 중 첫번째는 unique Key, 두번째는 비동기함수를 넣는다
 	 */
-	const { data, isLoading } = useQuery(["movies", "nowPlaying"], getMoives);
+	const { data, isLoading } = useQuery<IGetMoviesResult>(
+		["movies", "nowPlaying"],
+		getMoives
+	);
 	console.log(data, isLoading);
 	return <div></div>;
 }
